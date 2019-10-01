e1b5b92eb8f53e821010b8c2679ea3cdb73417a7
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2019-10-01 16:19:13 +0300

    SQL-testailua

--- application/clubs/models.py
+++ application/clubs/models.py
@@ -35,7 +35,7 @@ class Club(Base):
         stmt = text("SELECT club.id AS club_id, club.name, club.city, club.price, review.id AS review_id, review.account_id AS user_id, COUNT(review.grade) as reviews, ROUND(AVG(review.grade), 2) AS average "
                     "FROM club LEFT JOIN review ON review.club_id = club.id "
                     "GROUP BY club.id, review.id "
-                    "ORDER BY (CASE WHEN average is NULL THEN 1 ELSE 0 END), average DESC;")
+                    "ORDER BY (CASE WHEN ROUND(AVG(review.grade), 2) is NULL THEN 1 ELSE 0 END), average DESC;")
         result = db.engine.execute(stmt)
         
         return result
