4ad812dc4cba2fd61408e6c5669f8715417fbe8a
Juuso Järvenpää <juuso.kp.jarvenpaa@outlook.com>
2019-10-09 13:55:36 +0300

    Bcrypt otettu käyttöön

--- application/auth/views.py
+++ application/auth/views.py
@@ -4,6 +4,8 @@ from flask_login import login_user, logout_user
 from application import app, db, login_required
 from application.auth.models import User
 from application.auth.forms import LoginForm, CreateUserForm
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -14,12 +16,18 @@ def auth_login():
     if not form.validate():
         return render_template("auth/loginform.html", form = form)
 
-    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+    user = User.query.filter_by(username=form.username.data).first()
     if not user:
-        return render_template("auth/loginform.html", form=form, error = "No such username or password")
+        return render_template("auth/loginform.html", form=form, error = "No such username")
 
-    login_user(user)
-    return redirect(url_for("index"))
+    password = form.password.data
+    password_hash = user.password
+
+    if bcrypt.check_password_hash(password_hash, password):    
+        login_user(user)
+        return redirect(url_for("index"))
+
+    return render_template("auth/loginform.html", form=form, error= "Wrong password")    
 
 @app.route("/auth/logout")
 def auth_logout():
@@ -38,6 +46,9 @@ def auth_create():
 
     username = form.username.data
     email = form.email.data
+    password = form.password.data
+
+    password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
     # Varmistetaan, että username ja email ovat uniikkeja
     username_exists = User.query.filter_by(username=username).first()
@@ -51,7 +62,7 @@ def auth_create():
         return render_template("auth/newuserform.html", form=form, error="Username {0} and email {1} exist already".format(username, email))       
 
     # Luodaan uusi käyttäjä ja lisätään tietokantaann
-    new_user = User(form.name.data, form.email.data, form.username.data, form.password.data, form.role.data)
+    new_user = User(form.name.data, form.email.data, form.username.data, password_hash, form.role.data)
     db.session().add(new_user)
     db.session().commit()
 
